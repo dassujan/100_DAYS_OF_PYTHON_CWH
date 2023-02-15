9b6d1b8a8d3675d43403ba9bb8015bc467ad4935
SUJAN DAS <dassujan.005@gmail.com>
2023-02-15 22:34:42 +0530

    Intial commit

---
+++ 67 Day 67 - Exercise 6 Solution.py
@@ -0,0 +1,22 @@
+# Write a Library class with no_of_books and books as two instance variables. Write a program to create a library from this Library class and show how you can print all books, add a book and get the number of books using different methods. Show that your program doesnt persist the books after the program is stopped!
+
+class Library:
+    def __init__(self):
+        self.noBooks = 0
+        self.books = []
+    
+    def addBook(self, book):
+        self.books.append(book)
+        self.noBooks = len(self.books)
+
+    def showInfo(self):
+        print(f"The library has {self.noBooks} books. The books are")
+        for book in self.books:
+            print(book)
+
+
+l1 = Library()
+l1.addBook("Harry Potter1")
+l1.addBook("Harry Potter2")
+l1.addBook("Harry Potter3")
+l1.showInfo()
